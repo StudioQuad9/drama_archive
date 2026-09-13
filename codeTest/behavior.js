14b2c4032a4a95c727098efe0740497087f10bc0
Nobuyuki Takahiro <5891401+StudioQuad9@users.noreply.github.com>
2026-09-14 07:15:41 +0900

    教科書と練習コードを保存

---
+++ codeTest/behavior.js
@@ -0,0 +1,9 @@
+const hash = {
+  name: "takahiro",
+  job: "Web developer",
+};
+
+console.log(`${hash.job}変数を展開しつつスペースを入れたいのです${hash.job}`);
+
+// 外側の{ }：ここからJavaScriptですよ、というJSXの印
+// ${ }：文字列の中へ値を差し込むJavaScriptの書き方
