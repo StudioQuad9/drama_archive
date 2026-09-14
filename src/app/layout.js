29b431c47812bd8ba60c3a3db073e125523a2dd1
hokuto-ad05 <hokuto.ad05@gmail.com>
2026-09-14 16:11:53 +0900

    リセットCSSを追加する。

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
+import "@/styles/reset.css";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
